dfff2b35728a91df4926b7d57e1f00e2fb241265
Code Sandbox <hello@codesandbox.io>
2021-07-14 02:55:19 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.7235734caf39b7f3351c6dd8e7437275.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5dbb29b02e355b6c19ee37b5bae60d1",
+    "url": "https://edd-brother.github.io/csb-9r94q/index.html"
+  },
+  {
+    "revision": "c0ab4ac8339ee2ca7800",
+    "url": "https://edd-brother.github.io/csb-9r94q/static/css/main.93acf1c9.chunk.css"
+  },
+  {
+    "revision": "753169c77934e7b2c004",
+    "url": "https://edd-brother.github.io/csb-9r94q/static/js/2.c954dafb.chunk.js"
+  },
+  {
+    "revision": "eadc8ac8b6080df510389a008a76ce0a",
+    "url": "https://edd-brother.github.io/csb-9r94q/static/js/2.c954dafb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c0ab4ac8339ee2ca7800",
+    "url": "https://edd-brother.github.io/csb-9r94q/static/js/main.79b2780d.chunk.js"
+  },
+  {
+    "revision": "f1b5a12b32b2789ecffd",
+    "url": "https://edd-brother.github.io/csb-9r94q/static/js/runtime-main.0192c550.js"
+  }
+]);
